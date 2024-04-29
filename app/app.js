a36d0fc292b9bd0e398f2c264596e9018363cdb9
Your Name <you@example.com>
2024-04-29 18:57:24 +0000

    submit-podcaster route added

--- app/app.js
+++ app/app.js
@@ -131,15 +131,22 @@ app.post('/submit-listener', async (req, res) => {
     try {
         const { name, email, password } = req.body;
   
-        // Check if the email already exists in the database
+
     
-        res.redirect('/'); // Redirect to login page after successful signup
-    } catch (error) {
+        res.redirect('/');
       
     }
   });
   
+  app.post('/submit-podcaster', async (req, res) => {
+    try {
 
+        }
+
+     catch (error) {
+       
+    }
+  });
 // Start server on port 3000
 app.listen(3000, function () {
   console.log(`Server running at http://127.0.0.1:3000/`);
