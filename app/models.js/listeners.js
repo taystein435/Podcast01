3d6bfedd7d71fc6f5f4c9971fcba6bdff02fe979
Lubnae <lubnaamughal29@gmail.com>
2024-04-29 20:33:17 +0100

    Add User Method added to Listener Class

--- app/models.js/listeners.js
+++ app/models.js/listeners.js
@@ -10,9 +10,12 @@ class Listener {
 
     async addUser() {
         try {
-
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            const signupDate = new Date().toISOString().slice(0, 10);
+            const sql = "INSERT INTO Listeners (Name, Email, Password, SignupDate) VALUES (?, ?, ?, ?)";
+            await db.query(sql, [this.name, this.email, hashedPassword, signupDate]);
         } catch (error) {
-        
+            throw new Error("Error adding user to database");
         }
     }
 
